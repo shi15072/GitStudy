4233d1d4aaa5a531c9bc3003a53473080f756bea
shimm <2655244975@qq.com>
2021-08-25 20:59:57 +0800

    分支添加在第七行上

--- src/GitTest.java
+++ src/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
         System.out.println("git study2");
         System.out.println("git study3");
         System.out.println("有分支创建的");
+        System.out.println("分支在第七行上写代码");
     }
 }
